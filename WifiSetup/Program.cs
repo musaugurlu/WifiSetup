ed3982330403d5284f701a0a17b64dc477f6898b
Musa Ugurlu <mugurlu@rivercityscience.org>
2017-01-24 11:11:40 -0500

    Add project files.

---
+++ WifiSetup/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WifiSetup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"C:/QUSecure";
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("The path C:/QUSecure Exists Already");
+                return;
+            }
+
+            Directory.CreateDirectory(path);
+
+            // Write out the xml to a temporary file
+            System.IO.File.WriteAllText(@"C:\QUSecure\WLANExport.xml", WifiResource.WifiConf);
+
+
+            // Run netsh with the temp file as input 
+            System.Diagnostics.Process.Start("CMD.exe", "/C netsh wlan add profile filename=\"C:\\QUSecure\\WLANExport.xml\" & ping 127.0.0.1 -n 3 > nul & netsh wlan connect name=\"RCSA-Static\"");
+
+            // Remove the temp file
+            System.Threading.Thread.Sleep((int)System.TimeSpan.FromSeconds(2).TotalMilliseconds);
+            System.IO.File.Delete(@"C:\QUSecure\WLANExport.xml");
+
+            // Remove the temp folder
+
+            System.Threading.Thread.Sleep((int)System.TimeSpan.FromSeconds(2).TotalMilliseconds);
+            System.IO.Directory.Delete(@"C:\QUSecure");
+        }
+    }
+}
